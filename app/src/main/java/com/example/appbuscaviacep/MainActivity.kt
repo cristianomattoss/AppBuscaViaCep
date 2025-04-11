cc020e5d1786fd3ab11bbb28e22ad1c4a5c62d62
Cristiano Mattos <mateuscmattos@gmail.com>
2025-04-11 00:56:26 -0300

    implementa lógica de busca de CEP com Retrofit e exibição de dados via ViewBinding

--- app/src/main/java/com/example/appbuscaviacep/MainActivity.kt
+++ app/src/main/java/com/example/appbuscaviacep/MainActivity.kt
@@ -1,20 +1,105 @@
 package com.example.appbuscaviacep
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.appbuscaviacep.api.InterfaceEndereco
+import com.example.appbuscaviacep.api.RetrofitHelper
+import com.example.appbuscaviacep.api.model.Endereco
+import com.example.appbuscaviacep.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val enderecoAPI by lazy {
+        RetrofitHelper.retrofit.create(InterfaceEndereco::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        binding.buscarButton.setOnClickListener {
+            limparCampos()
+            CoroutineScope(Dispatchers.IO).launch {
+                val cep = binding.cepInput.text.toString()
+                verificarCep(cep)
+            }
         }
+
+    }
+
+    private fun limparCampos() {
+        with(binding){
+            logradouroText.text = getString(R.string.logradouro, "")
+            bairroText.text = getString(R.string.bairro, "")
+            cidadeText.text = getString(R.string.cidade, "")
+            estadoText.text = getString(R.string.estado, "")
+        }
+    }
+
+    private suspend fun verificarCep(cep: String) {
+        if (cep.length == 8 && cep.all { it.isDigit() }) {
+            buscarEndereco(cep)
+        }
+        else {
+            withContext(Dispatchers.Main) {
+                binding.cepInput.text?.clear()
+                Toast.makeText(this@MainActivity, "CEP inválido", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private suspend fun buscarEndereco(cep: String) {
+        val retorno: Response<Endereco>?
+
+        try {
+            retorno = enderecoAPI.buscarEndereco(cep)
+            val endereco = retorno.body()
+
+            withContext(Dispatchers.Main) {
+                if(endereco != null && retorno.isSuccessful && !verificarSeDadosSaoInvalidos(endereco)) {
+                    atribuirValor(endereco)
+                }
+                else {
+                    Toast.makeText(this@MainActivity, "Dados não encontrados", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }catch(e: Exception) {
+            e.printStackTrace()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Erro ao capturar dados", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    private fun verificarSeDadosSaoInvalidos(endereco: Endereco): Boolean {
+        return endereco.logradouro.isNullOrBlank() &&
+                endereco.bairro.isNullOrBlank() &&
+                endereco.cidade.isNullOrBlank() &&
+                endereco.estado.isNullOrBlank()
+    }
+
+    private fun atribuirValor(endereco: Endereco) {
+        with(binding) {
+            logradouroText.text = getString(R.string.logradouro, verificarValor(endereco.logradouro))
+            bairroText.text = getString(R.string.bairro, verificarValor(endereco.bairro))
+            cidadeText.text = getString(R.string.cidade, verificarValor(endereco.cidade))
+            estadoText.text = getString(R.string.estado, verificarValor(endereco.estado))
+        }
+    }
+
+    private fun verificarValor(valor: String?): String {
+        return if (valor.isNullOrBlank()) getString(R.string.invalido) else valor
     }
 }
--- app/src/main/java/com/example/appbuscaviacep/api/model/Endereco.kt
+++ app/src/main/java/com/example/appbuscaviacep/api/model/Endereco.kt
@@ -2,12 +2,10 @@ package com.example.appbuscaviacep.api.model
 
 import com.google.gson.annotations.SerializedName
 
-class Endereco {
-    data class Endereco(
+data class Endereco(
         val bairro: String,
         val estado: String,
         @SerializedName("localidade")
         val cidade: String,
         val logradouro: String
-    )
-}
+)
